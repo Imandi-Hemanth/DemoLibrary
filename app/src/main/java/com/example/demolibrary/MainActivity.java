eddc28d5855974999d2ca79386e412f01bc4bfee
Hemanth <himandi@ondevicesolutions.com>
2023-02-08 18:41:43 +0530

    demo library

--- app/src/main/java/com/example/demolibrary/MainActivity.java
+++ app/src/main/java/com/example/demolibrary/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.demolibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.demorslibrary.DisplayToast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisplayToast.s(MainActivity.this,"hello");
+            }
+        });
     }
 }
